57380ebfb5d858d4585f4ebccd8eec4975022b11
Norah Miriam <norahshibu@gmail.com>
2025-08-19 18:30:08 +0400

    Initialize frontend and modify project structure

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import ejs from 'ejs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.render('layout', {
+        title: 'Home',
+        activePage: 'home',
+        content: ejs.render(fs.readFileSync("views/homepage.ejs", "utf-8"))
+    });
+});
+
+app.get('/features', (req, res) => {
+    res.render('layout', {
+        title: 'Features',
+        activePage: 'features',
+        content: ejs.render(fs.readFileSync("views/features.ejs", "utf-8"))
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('layout', {
+        title: 'About', 
+        activePage: 'about',
+        content: ejs.render(fs.readFileSync("views/about.ejs", "utf-8"))
+    });
+});
+
+app.get('/contact', (req, res) => {
+    res.render('layout', {
+        title: 'Contact',
+        activePage: 'contact',
+        content: ejs.render(fs.readFileSync("views/contact.ejs", "utf-8"))
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`); 
+});
